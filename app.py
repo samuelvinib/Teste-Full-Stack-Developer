6c243e1f3e9452d66e6691c161a694d0078ba6fd
Samuel Vinícius Basílio <samuelvinib@gmail.com>
2024-02-07 15:10:20 -0300

    chore: template alterado com os inputs necessários para o projeto.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    triangle = request.form['triangle']
+    return f'Você enviou: {triangle}'
 
 
 if __name__ == '__main__':
